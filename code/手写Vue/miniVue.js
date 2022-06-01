c43d1117af33fc18f5e19e0a2bc90e1fddd561a8
vivices <573343189@qq.com>
2022-06-01 21:03:10 +0800

    feat: 手写Vue

--- code/手写Vue/miniVue.js
+++ code/手写Vue/miniVue.js
@@ -5,11 +5,18 @@ class MiniVue {
     this.$options = options || {}
     this.$data = options.data || {}
     this.$el = typeof options.$el === 'string' ? document.querySelector(options.$el) : options.$el
-    // 2. 把data中的成员转换成getter和setter并注入到vue实例中
-    this._proxyData(this.$data)
-    // 3. 调用observer对象，监听数据变化
+    /**
+     * 2. 把data中的成员转换成getter和setter并注入到vue实例中
+     *    这样就可以使用this.xxx来访问到this.data.xxx
+     *    
+     *    这里将data注入vue实例和Observer监听数据变化的区别有
+     *      * 代理的第一个对象不同，前者是this（vm）既vue实例，而后者传入的是data
+     */
+    this._proxyData(this.$data) 
+    // 3. 调用observer对象，监听数据变化 - 响应式数据核心
     new Observer(this.$data)
     // 4. 调用conpiler对象，解析指令和差值表达式
+
   }
 
   _proxyData(data) {
--- code/手写Vue/observer.js
+++ code/手写Vue/observer.js
@@ -14,7 +14,7 @@ class Observer {
    * @param {*} val - 属性对应的值，将此参数通过传递过来是为了避免在get里调用data[key]会触发死循环造成内存溢出报错
    */
   defineReactive(obj, key, val) {
-    const _this = this
+    // const _this = this
     // 如果val是对象，把val内部的属性变为响应式数据
     this.walk(val)
     Object.defineProperty(obj, key, {
@@ -23,10 +23,10 @@ class Observer {
       get() {
         return val
       },
-      set(newVal) {
+      set: (newVal) => {
         if (newVal === val) return
         // 如果data中的属性被重新赋值为新对象的时候，把新对象内部的属性变为响应式数据
-        _this.walk(newVal)
+        this.walk(newVal)
         val = newVal
         // 发送通知
       }
